2d50725a6a049d927b3aad31a9874a4ae822dc24
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-09-15 17:26:44 -0700

    create Click Listeners for Find Gyms and About App buttons  and toasts with appropriate messages

--- app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/MainActivity.java
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/MainActivity.java
@@ -2,12 +2,40 @@ package com.epicodus.annatimofeeva.myfitnesshelperversion1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mfindGymsButton;
+    private Button maboutAppButon;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        mfindGymsButton = (Button) findViewById(R.id.findGymsButton);
+        maboutAppButon = (Button) findViewById(R.id.aboutAppButon);
+
+        mfindGymsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "FirstActivity", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        maboutAppButon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Toast.makeText(MainActivity.this, "SecondActivity", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    } //end of OnCreate
 }
