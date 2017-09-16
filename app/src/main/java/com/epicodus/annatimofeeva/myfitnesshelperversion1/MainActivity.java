3c3cbf921bd3fcc0bb4e89c613e26a52b1713a51
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-09-15 19:30:49 -0700

    add gathering data from user input fuctionality

--- app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/MainActivity.java
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/MainActivity.java
@@ -3,19 +3,24 @@ package com.epicodus.annatimofeeva.myfitnesshelperversion1;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mfindGymsButton;
     private Button maboutAppButon;
+    private EditText mLocationEditText;
 
     @Override
         protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mLocationEditText = (EditText) findViewById(R.id.zipCode);
         mfindGymsButton = (Button) findViewById(R.id.findGymsButton);
         maboutAppButon = (Button) findViewById(R.id.aboutAppButon);
 
@@ -24,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 //Toast.makeText(MainActivity.this, "FirstActivity", Toast.LENGTH_SHORT).show();
+
+                String location = mLocationEditText.getText().toString();
+                Log.d(TAG, location);
 
                 Intent intent = new Intent(MainActivity.this, GymsActivity.class);
                 startActivity(intent);
