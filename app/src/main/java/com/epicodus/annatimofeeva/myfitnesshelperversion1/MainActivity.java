dad4e1d8856a9a7288b128f3826ecbecc83000fe
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-09-17 09:47:49 -0700

    View.OnClickListener interface implemented

--- app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/MainActivity.java
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/MainActivity.java
@@ -6,22 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements OnClickListener {
+
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mfindGymsButton;
     private Button maboutAppButon;
     private EditText mLocationEditText;
     private TextView mAppNameTextView;
-    
 
 
     @Override
-        protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -32,34 +33,46 @@ public class MainActivity extends AppCompatActivity {
         mfindGymsButton = (Button) findViewById(R.id.findGymsButton);
         maboutAppButon = (Button) findViewById(R.id.aboutAppButon);
 
-        mfindGymsButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        mfindGymsButton.setOnClickListener(this);
 
-                //Toast.makeText(MainActivity.this, "FirstActivity", Toast.LENGTH_SHORT).show();
-
-                String location = mLocationEditText.getText().toString();
-                Intent intent = new Intent(MainActivity.this, GymsActivity.class);
-                intent.putExtra("location", location);
-                startActivity(intent);
-
-            }
-        });
-
-        maboutAppButon.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                //Toast.makeText(MainActivity.this, "SecondActivity", Toast.LENGTH_SHORT).show();
-
-                Intent intent = new Intent(MainActivity.this, AboutActivity.class);
-                intent.putExtra("about", "The information about application will be provide later");
-                startActivity(intent);
-
-            }
-        });
+        maboutAppButon.setOnClickListener(this);
 
 
+    } //on Create
 
-    } //end of OnCreate
+    @Override
+
+    public void onClick(View view) {
+
+        //Toast.makeText(MainActivity.this, "FirstActivity", Toast.LENGTH_SHORT).show();
+        if(view == mfindGymsButton){
+
+        String location = mLocationEditText.getText().toString();
+        Intent intent = new Intent(MainActivity.this, GymsActivity.class);
+        intent.putExtra("location", location);
+        startActivity(intent);
+        }
+        if(view ==  maboutAppButon) {
+
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+        intent.putExtra("about", "The information about application will be provide later");
+        startActivity(intent);
+        }
+
+
+    }
+
+    //@Override
+
+//    public void onClick(View view) {
+//
+//        //Toast.makeText(MainActivity.this, "SecondActivity", Toast.LENGTH_SHORT).show();
+//
+//        Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+//        intent.putExtra("about", "The information about application will be provide later");
+//        startActivity(intent);
+//
+//    }
+
+
 }
