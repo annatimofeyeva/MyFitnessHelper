f854c4b121d9c176092747da71cdf7efb0039d7b
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-09-17 20:26:58 -0700

    model created, to UI Thread switching, succefully display Gym's data from Yelp API in the GymsActivity and in logcat

--- app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/GymsActivity.java
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/GymsActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 //import javax.security.auth.callback.Callback;
 
@@ -31,17 +32,19 @@ public class GymsActivity extends AppCompatActivity {
     @Bind(R.id.locationTextView) TextView mLocationTextView;
     @Bind(R.id.listView) ListView mListView;
 
-    private String[] gyms = new String[] {"Anytime Fitness", "The Seattle Gym", "Seattle Fitness Inc", "Seattle Authentic Club",
-            "24 Hours Fitness", "Gold's Gym"
-    };
+    public ArrayList<Gym> mGyms = new ArrayList<>();
 
-    private String[] description = new String[] {"We offer personal training and classes when you’re here, and the right tools to keep you on track when you’re not",
-    "The mission of TheSeattleGYM is to enhance the quality of life in the communities we serve and to instill the value and importance of health and fitness within our health club.",
-    "At Seattle Fitness, it is our mission to provide members the opportunity to experience fitness at its finest. Our goal is to share our knowledge and experience in a professional and friendly manner, contributing to your overall health and well-being.",
-    "We are dedicated to bring out the best in our members everyday by continuing to offer the very best in qualified professional fitness staff, excellent customer service, exciting fitness programs, and ever-evolving and improving fitness facilities and equipment.",
-    "Your workout should always be the high point of your day. Our Seattle gym is designed to excite and motivate, with amazing studio classes, innovative training programs, and ample workout space to help you get into your zone and get moving.",
-    "Today Gold’s Gym is the most recognized name in fitness serving more than 3 million members in 38 states and 22 countries around the world. Always at the forefront of the fitness revolution, Gold’s Gym has continually evolved its profile by equipping gyms with the best amenities and the latest in cardio and strength training equipment as well as the most dynamic group exercise programs including Zumba, yoga, group cycling, mixed martial arts, muscle endurance training, and Pilates."
-    };
+//    private String[] gyms = new String[] {"Anytime Fitness", "The Seattle Gym", "Seattle Fitness Inc", "Seattle Authentic Club",
+//            "24 Hours Fitness", "Gold's Gym"
+//    };
+//
+//    private String[] description = new String[] {"We offer personal training and classes when you’re here, and the right tools to keep you on track when you’re not",
+//    "The mission of TheSeattleGYM is to enhance the quality of life in the communities we serve and to instill the value and importance of health and fitness within our health club.",
+//    "At Seattle Fitness, it is our mission to provide members the opportunity to experience fitness at its finest. Our goal is to share our knowledge and experience in a professional and friendly manner, contributing to your overall health and well-being.",
+//    "We are dedicated to bring out the best in our members everyday by continuing to offer the very best in qualified professional fitness staff, excellent customer service, exciting fitness programs, and ever-evolving and improving fitness facilities and equipment.",
+//    "Your workout should always be the high point of your day. Our Seattle gym is designed to excite and motivate, with amazing studio classes, innovative training programs, and ample workout space to help you get into your zone and get moving.",
+//    "Today Gold’s Gym is the most recognized name in fitness serving more than 3 million members in 38 states and 22 countries around the world. Always at the forefront of the fitness revolution, Gold’s Gym has continually evolved its profile by equipping gyms with the best amenities and the latest in cardio and strength training equipment as well as the most dynamic group exercise programs including Zumba, yoga, group cycling, mixed martial arts, muscle endurance training, and Pilates."
+//    };
 
 
 
@@ -53,20 +56,20 @@ public class GymsActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
 
-        mListView = (ListView) findViewById(R.id.listView);
-        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
-
-
-        MyGymsArrayAdapter adapter = new MyGymsArrayAdapter(this, android.R.layout.simple_list_item_1, gyms, description);
-        mListView.setAdapter(adapter);
-
-        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String gym = ((TextView) view).getText().toString();
-                Toast.makeText(GymsActivity.this, gym, Toast.LENGTH_LONG).show();
-            }
-        });
+//        mListView = (ListView) findViewById(R.id.listView);
+//        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+//
+//
+//        MyGymsArrayAdapter adapter = new MyGymsArrayAdapter(this, android.R.layout.simple_list_item_1, gyms, description);
+//        mListView.setAdapter(adapter);
+//
+//        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                String gym = ((TextView) view).getText().toString();
+//                Toast.makeText(GymsActivity.this, gym, Toast.LENGTH_LONG).show();
+//            }
+//        });
 
 
         Intent intent = getIntent();
@@ -85,16 +88,49 @@ public class GymsActivity extends AppCompatActivity {
                     e.printStackTrace();
                 }
 
+//
+//                @Override
+//                public void onResponse(Call call, Response response) throws IOException {
+//                    try {
+//                        String jsonData = response.body().string();
+//                        if(response.isSuccessful()) {
+//                            Log.v(TAG, jsonData);
+//                            mGyms = yelpService.processResults(response);
+//                        }
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                    }
+//                }
 
                 @Override
-                public void onResponse(Call call, Response response) throws IOException {
-                    try {
-                        String jsonData = response.body().string();
-                        Log.v(TAG, jsonData);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
+                public void onResponse(Call call, Response response) {
+                    mGyms = yelpService.processResults(response);
+
+                    GymsActivity.this.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            String[] restaurantNames = new String[mGyms.size()];
+                            for (int i = 0; i < restaurantNames.length; i++) {
+                                restaurantNames[i] = mGyms.get(i).getName();
+                            }
+                            ArrayAdapter adapter = new ArrayAdapter(GymsActivity.this, android.R.layout.simple_list_item_1, restaurantNames);
+                            mListView.setAdapter(adapter);
+
+                            for (Gym gym : mGyms) {
+                                Log.d(TAG, "Name: " + gym.getName());
+                                Log.d(TAG, "Phone: " + gym.getPhone());
+                                Log.d(TAG, "Website: " + gym.getWebsite());
+                                Log.d(TAG, "Image url: " + gym.getImageUrl());
+                                Log.d(TAG, "Rating: " + Double.toString(gym.getRating()));
+                                Log.d(TAG, "Address: " + android.text.TextUtils.join(", ", gym.getAddress()));
+                                Log.d(TAG, "Categories: " + gym.getCategories().toString());
+                            }
+                        }
+                    });
                 }
+
+
 
 
 
--- app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/YelpService.java
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/YelpService.java
@@ -2,11 +2,19 @@ package com.epicodus.annatimofeeva.myfitnesshelperversion1;
 
 //import javax.security.auth.callback.Callback;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
 import okhttp3.Call;
 import okhttp3.Callback;
 import okhttp3.HttpUrl;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.Response;
 import se.akerfeldt.okhttp.signpost.OkHttpOAuthConsumer;
 import se.akerfeldt.okhttp.signpost.SigningInterceptor;
 
@@ -37,6 +45,54 @@ public class YelpService {
         call.enqueue(callback);
 
     }
+
+    public ArrayList<Gym> processResults(Response response) {
+        ArrayList<Gym> gyms = new ArrayList<>();
+
+        try {
+            String jsonData = response.body().string();
+            if (response.isSuccessful()) {
+                JSONObject yelpJSON = new JSONObject(jsonData);
+                JSONArray businessesJSON = yelpJSON.getJSONArray("businesses");
+                for (int i = 0; i < businessesJSON.length(); i++) {
+                    JSONObject restaurantJSON = businessesJSON.getJSONObject(i);
+                    String name = restaurantJSON.getString("name");
+                    String phone = restaurantJSON.optString("display_phone", "Phone not available");
+                    String website = restaurantJSON.getString("url");
+                    double rating = restaurantJSON.getDouble("rating");
+                    String imageUrl = restaurantJSON.getString("image_url");
+                    double latitude = restaurantJSON.getJSONObject("location")
+                            .getJSONObject("coordinate").getDouble("latitude");
+                    double longitude = restaurantJSON.getJSONObject("location")
+                            .getJSONObject("coordinate").getDouble("longitude");
+                    ArrayList<String> address = new ArrayList<>();
+                    JSONArray addressJSON = restaurantJSON.getJSONObject("location")
+                            .getJSONArray("display_address");
+                    for (int y = 0; y < addressJSON.length(); y++) {
+                        address.add(addressJSON.get(y).toString());
+                    }
+
+                    ArrayList<String> categories = new ArrayList<>();
+                    JSONArray categoriesJSON = restaurantJSON.getJSONArray("categories");
+
+                    for (int y = 0; y < categoriesJSON.length(); y++) {
+                        categories.add(categoriesJSON.getJSONArray(y).get(0).toString());
+                    }
+                    Gym gym = new Gym(name, phone, website, rating,
+                            imageUrl, address, latitude, longitude, categories);
+                    gyms.add(gym);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return gyms;
+    }
+
+
+
 
 }//end of class
 
