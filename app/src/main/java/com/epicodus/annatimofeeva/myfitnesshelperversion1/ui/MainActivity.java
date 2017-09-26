8becde5d94fa5e4382489e3a31dbb7f7bb5b1c57
Anna Timofeeva <anjatimofeyeva@gmail.com>
2017-09-25 17:21:24 -0700

    Firebase connected

--- app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/ui/MainActivity.java
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/ui/MainActivity.java
@@ -14,11 +14,15 @@ import android.widget.TextView;
 
 import com.epicodus.annatimofeeva.myfitnesshelperversion1.Constants;
 import com.epicodus.annatimofeeva.myfitnesshelperversion1.R;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity  implements OnClickListener {
 
-    private SharedPreferences mSharedPreferences;
-    private SharedPreferences.Editor mEditor;
+//    private SharedPreferences mSharedPreferences;
+//    private SharedPreferences.Editor mEditor;
+
+    private DatabaseReference mSearchedLocationReference;
 
     public static final String TAG = MainActivity.class.getSimpleName();
     private Button mfindGymsButton;
@@ -29,11 +33,19 @@ public class MainActivity extends AppCompatActivity  implements OnClickListener 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mSearchedLocationReference = FirebaseDatabase
+                .getInstance()
+                .getReference()
+                .child(Constants.FIREBASE_CHILD_SEARCHED_LOCATION);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        mEditor = mSharedPreferences.edit();
+//        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        mEditor = mSharedPreferences.edit();
+
+
 
 
         mAppNameTextView = (TextView) findViewById(R.id.appNameTextView);
@@ -58,9 +70,11 @@ public class MainActivity extends AppCompatActivity  implements OnClickListener 
         if (view == mfindGymsButton) {
 
             String location = mLocationEditText.getText().toString();
-            if(!(location).equals("")) {
-                addToSharedPreferences(location);
-            }
+//            if(!(location).equals("")) {
+//                addToSharedPreferences(location);
+//            }
+            saveLocationToFirebase(location);
+
             Intent intent = new Intent(MainActivity.this, GymsActivity.class);
             intent.putExtra("location", location);
             startActivity(intent);
@@ -73,9 +87,13 @@ public class MainActivity extends AppCompatActivity  implements OnClickListener 
         }
     }
 
-    private void addToSharedPreferences(String location) {
-        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+    public void saveLocationToFirebase(String location) {
+        mSearchedLocationReference.setValue(location);
     }
+
+//    private void addToSharedPreferences(String location) {
+//        mEditor.putString(Constants.PREFERENCES_LOCATION_KEY, location).apply();
+//    }
 
 }
 //@Override
