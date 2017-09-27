e9da0e63a9ab767fbe90da522297a2f9b7b75116
Guest User <Guest@epicodus-6.local>
2017-09-27 09:31:36 -0700

    add LoginActivity and CreateAccountActivity for user Auth

---
+++ app/src/main/java/com/epicodus/annatimofeeva/myfitnesshelperversion1/ui/CreateAccountActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.annatimofeeva.myfitnesshelperversion1.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.annatimofeeva.myfitnesshelperversion1.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
